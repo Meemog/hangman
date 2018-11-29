ddb584f04e9b3dcba8e45a9ed2d9ee352224f8c3
Meemog <36992704+Meemog@users.noreply.github.com>
2018-11-29 14:30:26 +0000

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,19 +1,39 @@
 import random
+import re
 
 ComputerWords = ["cat", "dog", "python", "programming", "computer", "binary", "hexadecimal", "monitor", "image", "mouse", "keyboard"]
 GameMode = "0"
+GameOver = False
 
 while GameMode != "1" and GameMode != "2":
-    GameMode = input("Choose one:\n1: 2 Players\n2: vs computer")
+    GameMode = input("Choose one:\n1: vs computer\n2: 2 players")
 
-    if GameMode == "1":
-        Word = input("Please enter a word")
     if GameMode == "2":
+        Word = input("Please enter a word")
+    if GameMode == "1":
         Word = ComputerWords[random.randint(0,len(ComputerWords))]
 
-Word.lower
-HiddenWord = list(Word)
-for i in range(0, len(Word)):
-    HiddenWord[i] = "_"
-"".join(HiddenWord)
+Word = Word.capitalize()
+HiddenWord = Word
+HiddenWord = list(HiddenWord)
+
+for i in range(0,len(Word)):
+    HiddenWord[i] = '_ '
+
+HiddenWord = "".join(HiddenWord)
 print(HiddenWord)
+
+GuessedLetters = []
+
+while GameOver != True:
+    GuessedLetter = input("Enter a letter")
+
+    if len(GuessedLetter) == 1:
+        Counter = 0
+
+        if GuessedLetter in GuessedLetters:
+            print("Letter has already been chosen")
+
+        else:
+            if GuessedLetter in Word:
+                print("yay")
