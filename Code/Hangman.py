eb983f5693f9932acd60b58e7bc0b86344e56640
Meemog <36992704+Meemog@users.noreply.github.com>
2018-12-02 15:17:25 +0000

    Update Hangman.py

--- Code/Hangman.py
+++ Code/Hangman.py
@@ -9,9 +9,31 @@ while GameMode != "1" and GameMode != "2":
     GameMode = input("Welcome to Hangman, You have 7 lives\n\nChoose one:\n1: vs computer\n2: 2 players")
 
     if GameMode == "2":
-        Word = input("Please enter a word")
+        WordPass = False
     if GameMode == "1":
         Word = ComputerWords[random.randint(0,len(ComputerWords)-1)]
+        WordPass = True
+
+FirstWordPass = False
+while WordPass == False:
+    WordPass = True
+
+    Word = input("Please enter a word\nOnly letters and spaces allowed")
+    for i in range(0, len(Word)):
+        if Word[i] == "/" or Word[i].isalpha():
+            FirstWordPass = True
+        else:
+            FirstWordPass = False
+
+        if Word[i] == " ":
+            Word = list(Word)
+            Word[i] = "/"
+            Word = "".join(Word)
+            FirstWordPass = True
+
+        if FirstWordPass == False:
+            WordPass = False
+
 
 Word = Word.upper()
 HiddenWord = Word
@@ -20,15 +42,20 @@ Word = list(Word)
 CheckWord = HiddenWord
 
 for i in range(0,len(Word)):
-    HiddenWord[i] = '*'
-    CheckWord[i] = '*'
+    if Word[i] == "/":
+        HiddenWord[i] = '/'
+        CheckWord[i] = '/'
+    else:
+        HiddenWord[i] = '*'
+        CheckWord[i] = '*'
 
 DisplayHiddenWord = HiddenWord
 DisplayHiddenWord = "".join(DisplayHiddenWord)
 print(str(DisplayHiddenWord))
 
 GuessedLetters = []
-counter = 0
+HitTotal = 0
+Win = False
 
 while GameOver != True:
     GuessedLetter = input("Enter a letter")
@@ -36,33 +63,47 @@ while GameOver != True:
 
     if len(GuessedLetter) == 1:
 
+        if GuessedLetter[0].isalpha():
 
-        if GuessedLetter in GuessedLetters:
-            print("Letter has already been chosen")
+            if GuessedLetter in GuessedLetters:
+                print("Letter has already been chosen")
 
-        else:
-            if GuessedLetter in Word:
-                for i in range(0, len(Word)):
-                    if Word[i] == GuessedLetter:
-                        HiddenWord[i] = GuessedLetter + " "
-                        CheckWord[i] = GuessedLetter
             else:
-                print("That letter is not in the word")
-                counter = counter + 1
-                print("You have " + str(7 - counter) + " lives left")
-            GuessedLetters.append(GuessedLetter)
+                if GuessedLetter in Word:
+                    for i in range(0, len(Word)):
+                        if Word[i] == GuessedLetter:
+                            HiddenWord[i] = GuessedLetter + " "
+                            CheckWord[i] = GuessedLetter
+                else:
+                    print("That letter is not in the word")
+                    HitTotal += 1
+                    print("You have " + str(7 - HitTotal) + " lives left")
+                GuessedLetters.append(GuessedLetter)
+        else:
+            print("Please only enter a letter")
     else:
-        print("Please only enter 1 letter")
+        if list(GuessedLetter) == Word:
+            Win = True
+        else:
+            print("Please only enter 1 letter")
 
     DisplayHiddenWord = "".join(HiddenWord)
     print(DisplayHiddenWord)
 
-    if counter == 7:
+    if HitTotal == 7:
         print("Game Over, You loose")
         GameOver = True
     if Word == CheckWord:
+        Win = True
+
+    if Win == True:
+        for i in range(0, len(Word)):
+            if Word[i] == "/":
+                Word[i] = " "
+
         Word = "".join(Word)
         print("Congratulations, You win. The word was " + Word)
         GameOver = True
+
 
 
